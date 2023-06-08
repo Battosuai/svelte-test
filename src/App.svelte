f0b8a42bd940e27ed6597cbe2286e64eae4173e9
MD. Asifur Rahman <asifur.rahman@brainstation-23.com>
2023-06-08 15:26:08 +0600

    Chapter 1

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main class="container">
+  <h1>Vite + Svelte</h1>
+
+  <div>
+    <Counter />
+  </div>
+</main>
+
+<style>
+  .container {
+    min-width: 100vw;
+  }
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  let name: string = "world";
+  let src: string = "https://picsum.photos/200/300";
+</script>
+
+<h1>Hello {name.toUpperCase()}</h1>
+
+<img
+  {src}
+  alt="lorem picsum"
+/>
+
+<style>
+  img {
+    height: 200px;
+    width: 200px;
+    object-fit: cover no-repeat;
+  }
+</style>
